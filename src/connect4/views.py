51995f3435c56752b6acec552ee19544377f0a3b
Daniel Brownridge <daniel@freshnewpage.com>
2016-05-14 12:25:35 +0100

    Minimal index page

--- src/connect4/views.py
+++ src/connect4/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    message = 'Connect4'
+    response = HttpResponse(message)
+    return response
