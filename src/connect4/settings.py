6c2f1e00ea6ac40f8a7e9f1099eee7669c5197fa
Daniel Brownridge <daniel@freshnewpage.com>
2016-05-14 09:46:43 +0100

    Updated settings to use PostgreSQL

--- src/connect4/settings.py
+++ src/connect4/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'connect4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'connect4',
+        'USER': 'danielbrownridge',
     }
 }
 
