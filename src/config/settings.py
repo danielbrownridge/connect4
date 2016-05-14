6a773700191ac6d3c3fe6d3b05c44b0a9d180020
Daniel Brownridge <daniel@freshnewpage.com>
2016-05-14 13:10:56 +0100

    Registering the connect4 app in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'connect4.apps.Connect4Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
