5eeafd0e0064454ae50280dbff37214253cd1384
Daniel Brownridge <daniel@freshnewpage.com>
2016-05-15 07:51:14 +0100

    Creating the model for games.

--- src/connect4/models.py
+++ src/connect4/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Game(models.Model):
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player_1')
+    player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player_2',
+            blank=True, null=True)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner',
+            blank=True, null=True)
+    finished = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
