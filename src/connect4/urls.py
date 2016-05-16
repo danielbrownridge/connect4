e35e882d02f293f7b65346f530cdfed1ed43818d
Daniel Brownridge <daniel@freshnewpage.com>
2016-05-16 20:42:08 +0100

    Rename url

--- src/connect4/urls.py
+++ src/connect4/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^games/$', views.GamesView.as_view(), name='games'),
-    url(r'^game/(?P<game_id>[0-9]+)/data/$', views.GameDataView.as_view(), name='game_data'),
+    url(r'^game/(?P<game_id>[0-9]+)/data/$', views.GameDataView.as_view(), name='game-data'),
     url(r'^play/(?P<game_id>[0-9]+)/$', views.PlayView.as_view(), name='play'),
 ]
